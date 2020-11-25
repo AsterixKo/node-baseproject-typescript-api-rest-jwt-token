a47bbd2683ded3d9096e1bfb5be8a809be42edc4
MSI\moneo <jvr.moneo@gmail.com>
2020-11-25 18:34:00 +0100

    Dar funcionalidad al tema de los roles

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import config from '../config/config.json';
+import { Role } from '../models/role.model';
 import { User } from '../models/user.model';
 
 class AuthController {
@@ -18,17 +19,25 @@ class AuthController {
 
         console.log('usuario:', usuario);
         if (usuario) {
-            // recordar que pasamos por post el email
-            const token = jwt.sign(
-                {
-                    email: req.body.email,
-                    password: req.body.password
-                },
-                config.jwtSecret,
-                { expiresIn: "1h" }
-            );
+            // recordar que pasamos por post el email y password
+            //buscamos el role
+            const roleOfuser = await Role.findOne({ where: { id: usuario.roleId } });
+            if (roleOfuser) {
+                const token = jwt.sign(
+                    {
+                        email: req.body.email,
+                        password: req.body.password,
+                        role: roleOfuser.name
+                    },
+                    config.jwtSecret,
+                    { expiresIn: "1h" }
+                );
 
-            res.send(token);
+                res.send(token);
+            }else{
+                res.sendStatus(404);
+            }
+
         } else {
             res.sendStatus(404);
         }
